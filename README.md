fc5585d238e3a8cb37e823095147498fe148840b
Alexander A. Gnatyna <a.gnatyna@gazeta.ru>
2017-08-17 15:39:31 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Template::Plugin::String::CRC32 - It's new $module
+
+# SYNOPSIS
+
+    use Template::Plugin::String::CRC32;
+
+# DESCRIPTION
+
+Template::Plugin::String::CRC32 is ...
+
+# LICENSE
+
+Copyright (C) Alexander A. Gnatyna.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Alexander A. Gnatyna <gnatyna@ya.ru>
---
+++ lib/Template/Plugin/String/CRC32.pm
@@ -0,0 +1,39 @@
+package Template::Plugin::String::CRC32;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Template::Plugin::String::CRC32 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Template::Plugin::String::CRC32;
+
+=head1 DESCRIPTION
+
+Template::Plugin::String::CRC32 is ...
+
+=head1 LICENSE
+
+Copyright (C) Alexander A. Gnatyna.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Alexander A. Gnatyna E<lt>gnatyna@ya.ruE<gt>
+
+=cut
+
